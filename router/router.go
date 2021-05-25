1be5ef65a83136f236391afc60a22e242460a820
jim0409 <berserker.01.tw@hotmail.com>
2021-05-26 01:52:07 +0800

    fix: fix cors bug from gin

--- router/router.go
+++ router/router.go
@@ -6,11 +6,29 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// https://stackoverflow.com/questions/29418478/go-gin-framework-cors
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func ApiRouter(r *gin.Engine) {
+	// https://stackoverflow.com/questions/29418478/go-gin-framework-cors
+	// middleware need to be implement before Group
+	r.Use(CORSMiddleware())
+
 	authrized := r.Group("/")
-
-	authrized.Use()
-
 	r1 := authrized.Group("/")
 	{
 		r1.POST("/note/:id", service.CreateNotes)
