a2d9eb057229f58ec0bf7b4bac0228b62ea94ec9
jim0409 <berserker.01.tw@hotmail.com>
2021-05-29 21:42:23 +0800

    misc: modify setup scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,3 @@
 
 # create an user `jim` with `markdb`
-docker exec -i mysql mysql -u root -psecret < ./models/mysqldb/sql/create_mysql_user_tb.sql
+docker exec -i mysql mysql -u root -psecret < ./scripts/create_mysql_user_tb.sql
